04d704e6ed4cd45c6ccc5d8979225d1c484ef720
Isuru-Jayasuriya <isuru.jayasuriya@inivossl.com>
2022-07-06 11:42:38 +0530

    smtp password changed

--- js/sendmail.js
+++ js/sendmail.js
@@ -2,7 +2,7 @@ function sendMail() {
   Email.send({
     Host: "smtp.elasticemail.com",
     Username: "isurutjayasuriya@gmail.com",
-    Password: "0B257831FC4BAC94E45B60DA439275EEB7E3",
+    Password: "68B8BA9116B9D44FF3444C4A4E7FE31EFF36",
     To: "isuru.jayasuriya@inivossl.com",
     From: document.getElementById("email").value,
     Subject: "New Customer Inquiry",
